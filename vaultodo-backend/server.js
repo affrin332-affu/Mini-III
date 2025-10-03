86f163973b7e38d01c902e7d3544b5acd6463d1a
ilakkiyaram05 <ilakkiyaram05@gmail.com>
2025-10-03 20:28:04 +0530

    Update server.js

--- vaultodo-backend/server.js
+++ vaultodo-backend/server.js
@@ -326,6 +326,6 @@ app.delete('/api/admin/tasks/:taskId', auth, isAdmin, async (req, res) => {
 });
 
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
